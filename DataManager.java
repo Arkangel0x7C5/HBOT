144df67cb280831b1efe722eba1857a403bef12b
[Zero] <edusvh@gmail.com>
2011-11-20 17:44:23 +0100

    Arreglado /load

--- DataManager.java
+++ DataManager.java
@@ -5,6 +5,12 @@ import java.net.URL;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
+
+import com.google.appengine.api.urlfetch.HTTPHeader;
+import com.google.appengine.api.urlfetch.HTTPRequest;
+import com.google.appengine.api.urlfetch.HTTPResponse;
+import com.google.appengine.api.urlfetch.URLFetchService;
+import com.google.appengine.api.urlfetch.URLFetchServiceFactory;
 import com.google.appengine.api.xmpp.JID;
 
 
@@ -23,7 +29,7 @@ public class DataManager
 	public int Save() throws Exception
 	{
 		Main.sender.sendEverybody("[BOT] Guardando contactos...");
-		String strURL="http://foro.h-sec.org/bot.php?key={contraseña}&value=";
+		String strURL="http://foro.h-sec.org/bot.php?key=hsb123423&value=";
 		
 		for(User u:mngUser.getUsers())
 		{
@@ -49,30 +55,29 @@ public class DataManager
 	}
 	
 	public int Load() throws Exception
-	{		
+	{		  
 		Main.sender.sendEverybody("[BOT] Cargando contactos...");
-		 URL url = new URL("http://foro.h-sec.org/bot.php?key={contraseña}");
-         BufferedReader reader=new BufferedReader(new InputStreamReader(url.openStream()));
-         String line;
-         String Buffer="";
-         while ((line=reader.readLine())!=null)
-         {
-        	 Buffer+=line;
-         }
-         reader.close();
-         
-                 
-         String[] lstUsers=Buffer.split("\\s");
-         
-         //mngUser.getUsers().clear();
-         
-         for(String s:lstUsers)
-         {
-        	 mngUser.addUser(new User(new JID(s+"/")));
-         }
-         
-         Main.sender.sendEverybody("[BOT] "+lstUsers.length+" contactos añadidos.");
-         
+		HTTPRequest request = new HTTPRequest(new URL("http://foro.h-sec.org/bot.php?key=hsb123423"));
+		request.setHeader(new HTTPHeader("Cache-Control", "no-cache,max-age=0"));
+		request.setHeader(new HTTPHeader("Pragma", "no-cache"));
+		URLFetchService urlFetchService=URLFetchServiceFactory.getURLFetchService();
+		HTTPResponse resp=urlFetchService.fetch(request);
+	
+		String Buffer=new String(resp.getContent());
+		
+            
+		String[] lstUsers=Buffer.split(" ");
+		 
+		//No es necesario, no se añaden duplicados
+		//mngUser.getUsers().clear();
+		 
+		for(String s:lstUsers)
+		{
+			mngUser.addUser(new User(new JID(s.trim()+"/")));
+		}
+ 
+		Main.sender.sendEverybody("[BOT] "+lstUsers.length+" contactos añadidos.");
+ 
 		return 0;
 	}
 }
