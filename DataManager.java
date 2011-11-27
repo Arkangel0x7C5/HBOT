aad2e3d6abbb67dfff9be01a8232e37d22af519a
Arkangel <arkangelhacket@gmail.com>
2011-11-27 19:31:45 +0100

    Tenia la version antigua que se conecta con h-sec

--- DataManager.java
+++ DataManager.java
@@ -1,18 +1,12 @@
-package hbot;
+ï»¿package hbot;
 
-import java.net.URL;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
 
-import com.google.appengine.api.urlfetch.HTTPHeader;
-import com.google.appengine.api.urlfetch.HTTPRequest;
-import com.google.appengine.api.urlfetch.HTTPResponse;
-import com.google.appengine.api.urlfetch.URLFetchService;
-import com.google.appengine.api.urlfetch.URLFetchServiceFactory;
 import com.google.appengine.api.xmpp.JID;
-
-
-//  AQUI HAY MUCHO QUE ARREGLAR !!
 
 public class DataManager
 {
@@ -25,56 +19,101 @@ public class DataManager
 	
 	public int Save() throws Exception
 	{
-		Main.sender.sendEverybody("[BOT] Guardando contactos...");
-		String strURL="http://foro.h-sec.org/bot.php?key={pass}&value=";
+		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
 		
-		for(User u:mngUser.getUsers())
+		Query q = new Query("users");
+		PreparedQuery pq = datastore.prepare(q);
+		boolean nuevo = false;
+		User NewUser=null;
+		for(User u: mngUser.getUsers())
 		{
-			strURL+=u.getAddr()+"%20";
+			for(Entity result: pq.asIterable())
+			{
+				NewUser=u;
+				if(result.getProperty("email").toString().compareTo(u.getAddr())==0)
+				{
+					result.setProperty("Nick", u.getNick());
+					if(u.isSnoozing())
+						result.setProperty("Snooze", "true");
+					else
+						result.setProperty("Snooze", "false");
+					
+					if(u.isMod())
+						result.setProperty("Mod", "true");
+					else
+						result.setProperty("Mod", "false");
+					
+					datastore.put(result);
+					nuevo=false;
+					break;
+				}
+				else nuevo=true;
+			}
+			if(nuevo)AddUser(NewUser);
 		}
-		
-		 URL url = new URL(strURL);
-         BufferedReader reader=new BufferedReader(new InputStreamReader(url.openStream()));
-         String line;
-         String Buffer="";
-         while ((line=reader.readLine())!= null)
-         {
-        	 Buffer+=line;
-         }
-         reader.close();
-         
-         if(Buffer.contains("OK"))
-         {
-        	 Main.sender.sendEverybody("[BOT] "+mngUser.getUsers().size()+" contactos guardados correctamente.");
-         }
-         
-        return 0;
+		return 0;
 	}
 	
+	@SuppressWarnings("deprecation")
 	public int Load() throws Exception
 	{		  
-		Main.sender.sendEverybody("[BOT] Cargando contactos...");
-		HTTPRequest request = new HTTPRequest(new URL("http://foro.h-sec.org/bot.php?key={pass}"));
-		request.setHeader(new HTTPHeader("Cache-Control", "no-cache,max-age=0"));
-		request.setHeader(new HTTPHeader("Pragma", "no-cache"));
-		URLFetchService urlFetchService=URLFetchServiceFactory.getURLFetchService();
-		HTTPResponse resp=urlFetchService.fetch(request);
-	
-		String Buffer=new String(resp.getContent());
+		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+		Query q = new Query("users");
+		PreparedQuery pq = datastore.prepare(q);
 		
-            
-		String[] lstUsers=Buffer.split(" ");
-		 
-		//No es necesario, no se añaden duplicados
-		//mngUser.getUsers().clear();
-		 
-		for(String s:lstUsers)
+		//TODO: Actualizar deprecated
+		if(pq.countEntities()==0)
 		{
-			mngUser.addUser(new User(new JID(s.trim()+"/")));
+			Entity FirstUser=new Entity("users");
+			FirstUser.setProperty("Nick","Arkangel");
+			FirstUser.setProperty("email","arkangelhacket@gmail.com");
+			FirstUser.setProperty("Snooze","false");
+			FirstUser.setProperty("Mod","true");
+			datastore.put(FirstUser);
 		}
- 
-		Main.sender.sendEverybody("[BOT] "+lstUsers.length+" contactos añadidos.");
- 
+				
+		for(Entity result: pq.asIterable())
+		{
+			mngUser.addUser(new User(new JID(result.getProperty("email").toString()+"/")));
+			for(User u: mngUser.getUsers())
+			{
+				if(result.getProperty("email").toString().compareTo(u.getAddr())==0)
+				{
+				u.setNick(result.getProperty("Nick").toString());
+				if(result.getProperty("Snooze").toString().compareTo("true")==0)u.SetSnooze(true);
+				else u.SetSnooze(false);
+				if(result.getProperty("Mod").toString().compareTo("true")==0)u.SetMod(true);
+				else u.SetMod(false);
+				}
+			}
+		}
 		return 0;
+	}
+	
+	public int remove(String msg)
+	{
+		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+		Query q = new Query("users");
+		q.addFilter("email", Query.FilterOperator.EQUAL, msg);
+		PreparedQuery pq = datastore.prepare(q);
+		
+		for(Entity user: pq.asIterable())
+		{
+			datastore.delete(user.getKey());
+		}
+		return 0;
+	}
+	
+	private void AddUser(User user)
+	{
+		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+		Entity users = new Entity("users");
+		users.setProperty("Nick", user.getNick());
+		users.setProperty("email", user.getAddr());
+		if(user.isSnoozing())users.setProperty("Snooze", "true");
+		else users.setProperty("Snooze", "false");
+		if(user.isMod())users.setProperty("Mod", "true");
+		else users.setProperty("Mod", "false");
+		datastore.put(users);
 	}
 }
